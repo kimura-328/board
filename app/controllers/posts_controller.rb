0b19ef14bc4ab5bd89a4bb28d0c00b0d30f8a609
kimura-328 <160104523+kimura-328@users.noreply.github.com>
2024-05-04 19:36:20 +0000

    ログインしたユーザは自分の投稿の編集削除ができる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:index, :new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   def index
     @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -9,7 +14,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to posts_path
     else
@@ -35,6 +40,8 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to posts_path
   end
+  
+  
 
   def logged_in_user
     unless current_user
@@ -49,4 +56,8 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :content)
   end
 
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to posts_path if @post.nil?
+  end
 end
